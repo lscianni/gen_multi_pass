c3c5fd7be5dca517a43d699b947ed6997bb34bf4
lscianni <lscianniit@gmail.com>
2019-03-10 18:11:46 -0400

    fix license

--- gen_multi_pass.py
+++ gen_multi_pass.py
@@ -5,9 +5,24 @@
   8 characters one capital letter one special character
   and one integer
   11/27/2017
-  Louis Scianni
+
+Copyright (C) 2019  Louis Scianni
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
  
- released under GLP-v2
 """
 
 import openpyxl
